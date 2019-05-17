62a9291e054078d04af2625711bdf7d8bc938c2a
Eugene Peluhnya <peluhnya@outlook.com>
2019-05-17 09:49:01 +0300

    Add

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module TrauminselreisenDashboard
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.available_locales = [:en, :de]
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
     config.hosts << "dashboard.iteasycode.com"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
