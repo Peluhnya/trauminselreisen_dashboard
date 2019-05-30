04181018d685c5f304f8684bc6bde202f51923d7
Eugene Peluhnya <peluhnya@outlook.com>
2019-05-30 13:29:28 +0300

    database

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -145,8 +145,10 @@ class ApplicationRecord < ActiveRecord::Base
           twomonth_i = I18n.t("date.abbr_month_names").compact.index twomonth
           ms = I18n.t("date.abbr_month_names").compact
           ms.shift(onemonth_i)
-          unless twomonth == 12 and twomonth.nil?
-            ms.pop(11-twomonth_i)
+          unless twomonth == 12
+            unless twomonth.nil?
+              ms.pop(11-twomonth_i)
+            end
           end
 
         end
